65c036f4312273bf2ccc042598be36f0fc97195f
JOOYUNHAK <wndbsgkr@naver.com>
2025-07-18 22:21:37 +0900

    feat: swagger 추가

--- apps/api/src/api.controller.ts
+++ apps/api/src/api.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ApiService } from './api.service';
+import { ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('root')
 @Controller()
 export class ApiController {
   constructor(private readonly apiService: ApiService) {}
 
-  @Get()
-  getHello(): string {
+  @ApiOperation({
+    summary: '테스트 API',
+    description: '테스트를 위한 API입니다.',
+  })
+  @ApiParam({ name: 'id', description: '사용자 ID' })
+  @Get(':id')
+  getHello(@Param('id') id: number): string {
     return this.apiService.getHello();
   }
 }
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,7 @@ import { getValidationPipeOptions } from '@libs/common';
 import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
 import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule, { bufferLogs: true });
@@ -23,6 +24,17 @@ async function bootstrap() {
   });
 
   app.enableShutdownHooks(); // for mikroorm
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Swagger example')
+    .setDescription('The test API description')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('docs', app, documentFactory, { useGlobalPrefix: true });
 
   await app.listen(process.env.PORT ?? 3000);
 }
