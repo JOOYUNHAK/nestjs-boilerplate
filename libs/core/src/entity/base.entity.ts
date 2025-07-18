85625670537675ed32d90b7d43d09475b7b1c24d
JOOYUNHAK <wndbsgkr@naver.com>
2025-07-18 20:28:37 +0900

    chore: mikro-orm schema create 스크립트 추가

--- libs/core/src/entity/base.entity.ts
+++ libs/core/src/entity/base.entity.ts
@@ -1,17 +1,7 @@
-import {
-  Entity,
-  Opt,
-  OptionalProps,
-  PrimaryKey,
-  PrimaryKeyProp,
-  Property,
-} from '@mikro-orm/core';
+import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
 
 @Entity({ abstract: true })
 export class BaseEntity {
-  [PrimaryKeyProp]?: 'id';
-  [OptionalProps]?: 'updatedAt' | 'deletedAt';
-
   @PrimaryKey({ autoincrement: true })
   id!: number;
 
@@ -26,11 +16,11 @@ export class BaseEntity {
     onUpdate: () => new Date(),
     nullable: true,
   })
-  updatedAt: Opt<Date>;
+  updatedAt?: Date;
 
   @Property({
     type: 'timestamptz',
     nullable: true,
   })
-  deletedAt: Opt<Date>;
+  deletedAt?: Date;
 }
