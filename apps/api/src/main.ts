1609267b46ea58eb1d2c1778b3962cb8f74140f7
JOOYUNHAK <wndbsgkr@naver.com>
2025-07-18 21:49:14 +0900

    feat: app cors 추가

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,7 @@ import { ApiModule } from './api.module';
 import { ValidationPipe } from '@nestjs/common';
 import { getValidationPipeOptions } from '@libs/common';
 import { Logger } from 'nestjs-pino';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule, { bufferLogs: true });
@@ -11,6 +12,14 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe(getValidationPipeOptions()));
   app.useLogger(app.get(Logger));
+
+  const configService = app.get(ConfigService);
+
+  app.enableCors({
+    origin: configService.getOrThrow('origin'),
+  });
+
+  app.enableShutdownHooks(); // for mikroorm
 
   await app.listen(process.env.PORT ?? 3000);
 }
