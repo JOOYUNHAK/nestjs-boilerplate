436c176935bca7024913ec65df3ab1baf0f4b914
JOOYUNHAK <wndbsgkr@naver.com>
2025-08-03 18:20:23 +0900

    fix(common): validationPipe boolean 변환 오류 수정

--- libs/common/src/validators/boolean-validator.decorator.ts
+++ libs/common/src/validators/boolean-validator.decorator.ts
@@ -23,11 +23,15 @@ export function BooleanValidator(
     decorators.push(IsNotEmpty());
   }
 
+  // ValidationPipe의  enableImplicitConversion 옵션때문에
+  // 문자열을 항상 true로 반환한다.
+  // 이 문제를 해결하기 위해 기존 값에 접근하여 변환
+  // @see
   decorators.push(
-    Transform(({ value }) => {
-      if (value === 'true') return true;
-      if (value === 'false') return false;
-      return value;
+    Transform(({ obj, key }) => {
+      if (obj[key] === 'true') return true;
+      if (obj[key] === 'false') return false;
+      return obj[key];
     }),
   );
 
